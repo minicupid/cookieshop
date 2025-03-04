0a4f6797bc8db372e9fd8887dc4abe889e286ad5
minicupid <jl16358@nyu.edu>
2025-03-04 11:07:03 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# credits
+
+* original cookie icons and layout by me
+* header art by uncredited owner on pinterest
+* background music is 'herbal tea' by shushubobo
+* some code was referenced from forums and tutorials
---
+++ script.js
@@ -0,0 +1,18 @@
+var footer = document.querySelector(".footer");
+var audio = document.getElementById("crunch");
+var audio = document.getElementById("music");
+
+var display = true;
+
+function hideShow() {
+    if (display) {
+      footer.style.display = "none";
+      crunch.play();
+      music.play();
+
+    } else {
+
+      footer.style.display = "block";
+    }
+    display = !display;
+  }
