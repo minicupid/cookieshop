4299072cb5da2d018d865a4b97c8c789c71087d2
minicupid <jl16358@nyu.edu>
2025-03-12 02:23:53 -0400

    v5.5 minor changes regarding sound effects

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ var returnButton = purchase.querySelector(".return");
 var consumed = [];
 var musicplaying = false;
 
+returnButton.style.display = "none";
+
+var display = true;
+var displayedCookie = "";
+var bites = 0;
+
 window.onload = function() {
   window.scrollTo({
       top: document.body.scrollHeight,
@@ -17,28 +23,19 @@ window.onload = function() {
   });
 }
 
-// if autoplay not work
-
-document.addEventListener('mousemove', function() {
+document.addEventListener('click', function() {
     if (!musicplaying) {
         music.play();
         musicplaying = true;
     }
 });
 
-returnButton.style.display = "none";
-
-var display = true;
-var displayedCookie = "";
-var bites = 0;
-
 // hide and show footer ===================================================== 
 
 function hideShow() {
   if (display) {
     footer.style.display = "none";
-    crunch.play();
-    music.play();
+    boop.play();
   }
   else {
     footer.style.display = "block";
@@ -46,12 +43,12 @@ function hideShow() {
   display = !display;
 }
 
-// item click listener to prompt purchase div =============================================
+// item click listener -> bite cookie =============================================
 
 document.querySelectorAll(".item").forEach(item => {
   item.addEventListener("click", function() {
-      const name = this.querySelector(".name").textContent.trim();
-      showPurchase(name);
+      const name = this.querySelector(".name").textContent.trim(); // get cookie
+      showPurchase(name); // show purchase div w selected cookie
   });
 });
 
@@ -63,7 +60,7 @@ function biteCookie() {
     if (!displayedCookie) return;
     
     bites++;
-    crunch.play();
+    new Audio(crunch.src).play();
     
     if (bites === 1) {
         cookieImage.src = `assets/${displayedCookie}2.png`;
@@ -107,7 +104,7 @@ function showPurchase(selectedCookie) {
 
 }
 
-// consumed cookies =====================================================
+// consumed cookies (wip) =====================================================
 
 function showConsumed() {
   console.log("cookies consumed:", consumed);
